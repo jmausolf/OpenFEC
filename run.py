29cdccc57fe879916d375839c884a8db055de77c
jmausolf <jgm346@gmail.com>
2017-10-31 23:15:15 -0500

    adding code for getPARTY

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
 from getFEC import *
+from getPARTY import *
 from random import random
 import traceback
 
@@ -9,30 +10,8 @@ class openFEC:
 		self.years = years
 
 
-	#Combine = Collapse/Remove Across Years
-	def combine(companies, name=""):
-		for company in companies:
-			try:
-				print("{}\n[*] Combining yearly data for {}".format("--"*20, company))
-				collapse_signature = collapse_csvs(company, "schedule a", None, name)
-				remove_files(collapse_signature)
-			except Exception as exc:
-				print(traceback.format_exc())
-				print(exc)
-				pass
-
-	def dedupe(companies, column=None):
-		for company in companies:
-			try:
-				print("{}\n[*] Combining/deduping data for {}".format("--"*20, company))
-				collapse_signature = dedupe_merged_csvs(company, column)
-				remove_files(collapse_signature)
-			except Exception as exc:
-				print(exc)
-				pass
-
 	#Run = Download, Collapse, Remove
-	def run(companies, years):
+	def getFEC(companies, years):
 
 		base_sleep_time = 1
 
@@ -78,6 +57,75 @@ class openFEC:
 				finally:
 					pass
 
+	#Combine = Collapse/Remove Across Years
+	def combine(companies, name=""):
+		for company in companies:
+			try:
+				print("{}\n[*] Combining yearly data for {}".format("--"*20, company))
+				collapse_signature = collapse_csvs(company, "schedule a", None, name)
+				remove_files(collapse_signature)
+			except Exception as exc:
+				print(traceback.format_exc())
+				print(exc)
+				pass
+
+	#Dedupe = Combine Multiple Attempts of Run
+	def dedupe(companies, column=None):
+		for company in companies:
+			try:
+				print("{}\n[*] Combining/deduping data for {}".format("--"*20, company))
+				collapse_signature = dedupe_merged_csvs(company, column)
+				remove_files(collapse_signature)
+			except Exception as exc:
+				print(exc)
+				pass
+
+	#Get Party ID
+	#Run = Download, Collapse, Remove
+	def getPARTY(companies):
+
+		base_sleep_time = 1
+
+		def tasks(company, test_break=False):
+			if test_break is True:
+				print("[*] BROKEN")
+				len(p) #intentional error to test break
+			else:
+				#tasks
+				filenames = getPARTY(company)
+				merge_contrib_pid(filenames)
+				return
+
+
+		def retry(company):
+				try:
+					tasks(company) #not broken
+					return
+				except:
+					for attempt in range(1, 10+1):
+						try:
+							print("[*] ERROR COLLECTING PARTY IDS FOR {}...attempt {}".format(company, attempt))
+							time.sleep(pow(2, attempt) * base_sleep_time * random())
+							tasks(company)
+							return
+						except:
+							if attempt>=10:
+								print("[*] MAXIMUM retries exceeded...FINAL ERROR COLLECTING {}".format(company))
+				finally:
+					pass
+
+
+		for company in companies:
+			print("{}\n[*] {} {}".format("--"*20, company))
+			try:
+				retry(company)
+			except:
+				d = 3600
+				print("[*] Possible rate limiting, trying again in {} minutes".format(d/60))
+				time.sleep(d)
+				retry(company)
+			finally:
+				pass
 
 
 ############################################################################
@@ -92,6 +140,7 @@ years = ["2016", "2012", "2008", "2004", "2000", "1996", "1992", "1988", "1984"]
 #companies = ["Walmart"]
 #companies = ["General Electric"]
 #companies = ["Apple"]
+companies = ["Exxon Mobile"]
 
 #companies = ["Goldman Sachs"]
 #years = ["2008"]
@@ -101,13 +150,12 @@ years = ["2016", "2012", "2008", "2004", "2000", "1996", "1992", "1988", "1984"]
 
 
 #dedupe multiple runs (need to remove json/dict column)
-#dedupe_merged_csvs("Goldman Sachs", "committee")
-openFEC.dedupe(companies, "committee")
+#openFEC.dedupe(companies, "committee")
 
-#openFEC.run(companies, years)
+#openFEC.getFEC(companies, years)
 #openFEC.combine(companies)
 
-
+openFEC.getPARTY(companies)
 
 ############################################################################
 ## STEP 2: Get All Schedule A for Companies
