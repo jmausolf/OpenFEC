e77c636bcb3dd89ef85b08dab8a68edf2f148f22
jmausolf <jgm346@gmail.com>
2017-11-08 16:47:02 -0600

    adding example credentials and function

---
+++ example_credentials.py
@@ -0,0 +1,23 @@
+#DEFINE YOUR API KEYS
+ak1 = "supersecret1"
+ak2 = "supersecret2"
+ak3 = "supersecret3"
+ak4 = "supersecret4"
+ak5 = "supersecret5"
+
+api_keys = [ak1, ak2, ak3, ak4, ak5]
+
+def get_api_key(api_keys):
+	used = []
+	for key in api_keys:
+		yield key
+		used.append(key)
+	while used:
+		for key in used:
+			yield key
+
+#CALL IN THE SCRIPT TO CYCLE KEYS
+#newkey = get_api_key(api_keys)
+
+
+
