5a4c9552be3ccb049f70f3f9e90dfea5481e4233
jmausolf <jgm346@gmail.com>
2018-02-18 16:09:42 -0600

    updating companies

--- config.py
+++ config.py
@@ -1,11 +1,11 @@
 #companies = ["Exxon", "Microsoft", "General Motors", "Citigroup", "Goldman Sachs", "Walmart", "Marathon Oil", "Apple", "Berkshire Hathaway", "Amazon", "Boeing", "Home Depot", "Ford Motor", "Kroger", "Chevron", "Wells Fargo", "CVS"]
-#companies = ["Walmart", "Exxon Mobile", "Marathon Oil", "Apple", "Berkshire Hathaway", "Amazon", "Boeing", "Alphabet", "Home Depot", "Ford Motor", "Kroger", "Chevron", "Morgan Chase", "Wells Fargo"]
-#years = ["2016", "2012", "2008", "2004", "2000", "1996", "1992", "1988", "1984"]
+companies = ["Walmart", "Exxon Mobile", "Marathon Oil", "Apple", "Berkshire Hathaway", "Amazon", "Boeing", "Alphabet", "Home Depot", "Ford Motor", "Kroger", "Chevron", "Morgan Chase", "Wells Fargo"]
+years = ["2016", "2012", "2008", "2004", "2000", "1996", "1992", "1988", "1984"]
 
 #companies = ["Exxon Mobile", "Amazon", "Boeing"]
 #companies = ["Walmart"]
 #companies = ["General Electric"]
-companies = ["Apple"]
+#companies = ["Apple"]
 #companies = ["Exxon"]
 #companies = ["Microsoft"]
 
