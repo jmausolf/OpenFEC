f2415f89d2694b6bf79ccce9a610898871db3d46
jmausolf <jgm346@gmail.com>
2018-02-21 01:54:58 -0600

    updates to db script

--- fec_download/build_db.py
+++ fec_download/build_db.py
@@ -23,7 +23,12 @@ def insert_file_into_table(cursor, sql_script, file, sep=','):
 	qry = open(sql_script, 'rU').read()
 	csvReader = csv.reader(open(file), delimiter=sep, quotechar='"')
 	for row in csvReader:
-		cursor.execute(qry, row)
+		try:
+			cursor.execute(qry, row)
+		except sqlite3.IntegrityError as e:
+			#print(e)
+			pass
+			#update script
 
 
 #helper functions
@@ -46,28 +51,37 @@ def ret_script_file(action, file):
 
 
 
-db = connect_db("test.db")
+db = connect_db("openFEC.db")
 c = db.cursor()
 
 
 create_table(c, "create_tables.sql")
 
+#insert oldest first, update with newer?
+#insert_file_into_table(c, 'insert_cm.sql', 'downloads/cm14_fec_2018-02-19_cm.txt', '|')
+#insert_file_into_table(c, 'insert_cm.sql', 'downloads/cm16_fec_2018-02-19_cm.txt', '|')
 
-insert_file_into_table(c, 'insert_cm.sql', 'downloads/cm14_fec_2018-02-19_cm.txt', '|')
+#try indv
+#insert_file_into_table(c, 'insert_indiv.sql', 'downloads/indiv14_fec_2018-02-19_itcont.txt', '|')
 
 
 #eventually
-#files = ret_files("downloads/", "txt")
-#for file in files:
-#	sql_script = ret_script_file("insert", file)
-#	insert_file_into_table(c, sql_script, file, '|')
+files = ret_files("downloads/", "txt")
+for file in files:
+	sql_script = ret_script_file("insert", file)
+	insert_file_into_table(c, sql_script, file, '|')
 
 
 
 
-for row in c.execute('SELECT * FROM committee_master'):
-	print(row)
+#for row in c.execute('SELECT * FROM committee_master'):
+#	print(row)
 
+for result in c.execute("SELECT COUNT(*) FROM committee_master;"):
+	print(result)
+
+for result in c.execute("SELECT COUNT(*) FROM individual_contributions;"):
+	print(result)
 
 
 exit_db(db)
--- fec_download/create_tables.sql
+++ fec_download/create_tables.sql
@@ -1,4 +1,3 @@
-
 DROP TABLE if exists committee_master;
 
 CREATE TABLE committee_master (
@@ -19,3 +18,34 @@ CREATE TABLE committee_master (
 	cand_id TEXT
 	);
 
+CREATE UNIQUE INDEX idx_committee_master ON committee_master (cm_id);
+
+
+
+DROP TABLE if exists individual_contributions;
+
+CREATE TABLE individual_contributions (
+	cm_id TEXT NOT NULL,
+	amendment_ind TEXT,
+	report_type TEXT,
+	transaction_pgi TEXT,
+	image_num TEXT,
+	transaction_type TEXT,
+	entity_type TEXT,
+	name TEXT,
+	city TEXT,
+	state TEXT,
+	zip_code TEXT,
+	employer TEXT,
+	occupation TEXT,
+	transaction_date TEXT,
+	transaction_amount TEXT,
+	other_id TEXT,
+	transaction_id TEXT,
+	file_num NUMERIC,
+	memo_code TEXT,
+	memo_text TEXT,
+	sub_id NUMERIC NOT NULL
+	);
+
+CREATE UNIQUE INDEX idx_individual_contributions ON individual_contributions (sub_id);
