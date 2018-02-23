bda6e560c33846ea4968b344569a28d3b6e725fa
jmausolf <jgm346@gmail.com>
2018-02-23 13:58:53 -0600

    adding streamlined expandable download functions

--- fec_download/download.py
+++ fec_download/download.py
@@ -7,7 +7,7 @@ from glob import glob
 import re
 import zipfile
 import os
-from setlogger import *
+#from setlogger import *
 
 #Get Date for Filenames
 now = datetime.datetime.now()
@@ -16,30 +16,38 @@ date = now.strftime("%Y-%m-%d")
 #Specify File Key (Source) and Value [download url]
 #table name == dictionary name
 
+def get_download_url(year, type_key):
+	base_url = "https://cg-519a459a-0ea3-42c2-b7bc-fa1143481f74.s3-us-gov-west-1.amazonaws.com/bulk-downloads"
+	key = "{}{}".format(type_key, year[2:4])
+	file = "{}.zip".format(key)
+	#print(file)
+	url = "{}/{}/{}".format(base_url, year, file)
+	#print(url)
 
-#Data Type: Committee Master File
-committee_master = {
-	'cm16' : ['cm', 'committee_master', 'https://cg-519a459a-0ea3-42c2-b7bc-fa1143481f74.s3-us-gov-west-1.amazonaws.com/bulk-downloads/2016/cm16.zip'],
-	'cm14' : ['cm', 'committee_master', 'https://cg-519a459a-0ea3-42c2-b7bc-fa1143481f74.s3-us-gov-west-1.amazonaws.com/bulk-downloads/2014/cm14.zip'],
-}
+	return [key, url]
 
-#Data Type: Candidate Master File
-candidate_master = {
-	'cn16' : ['cn', 'candidate_master', 'https://cg-519a459a-0ea3-42c2-b7bc-fa1143481f74.s3-us-gov-west-1.amazonaws.com/bulk-downloads/2016/cn16.zip']
-}
 
-#Data Type: Candidate Committee Linkage File
-cand_cmte_link = {
-	'ccl16': ['ccl', 'cand_cmte_link', 'https://cg-519a459a-0ea3-42c2-b7bc-fa1143481f74.s3-us-gov-west-1.amazonaws.com/bulk-downloads/2016/ccl16.zip']
-}
 
-#Data Type: Individual Contributions
-individual_contributions = {
-	#'indiv16' : ['indiv', 'individual_contributions', 'https://cg-519a459a-0ea3-42c2-b7bc-fa1143481f74.s3-us-gov-west-1.amazonaws.com/bulk-downloads/2016/indiv16.zip'],
-	#'indiv14' : ['indiv', 'individual_contributions', 'https://cg-519a459a-0ea3-42c2-b7bc-fa1143481f74.s3-us-gov-west-1.amazonaws.com/bulk-downloads/2014/indiv14.zip'],
-	#'indiv12' : ['indiv', 'individual_contributions', 'https://cg-519a459a-0ea3-42c2-b7bc-fa1143481f74.s3-us-gov-west-1.amazonaws.com/bulk-downloads/2012/indiv12.zip'],
-	'indiv10' : ['indiv', 'individual_contributions', 'https://cg-519a459a-0ea3-42c2-b7bc-fa1143481f74.s3-us-gov-west-1.amazonaws.com/bulk-downloads/2010/indiv10.zip']
-}
+#x = get_download_url("2016", "cm")
+#y = get_download_url("2016", "ccl")
+
+#print(x)
+
+def make_download_table_dicts(years, type_key, table_key):
+	output = {}
+
+	for year in years:
+		key_url = get_download_url(year, type_key)
+		key = key_url[0]
+		url = key_url[1]
+		table = table_key[type_key]
+
+		output[key] = [type_key, table, url]
+		
+	#print(output)
+	return output
+
+
 
 table_key = {
 	'cm' : 'committee_master',
@@ -48,8 +56,27 @@ table_key = {
 	'indiv' : 'individual_contributions'
 }
 
-#All Reports
-datasets = [committee_master, candidate_master, cand_cmte_link, individual_contributions]
+
+#make_download_table_dicts(['2014', '2016'], "cm", table_key)
+
+#committee_master = make_download_table_dicts(['2014', '2016'], "cm", table_key)
+
+#print(committee_master)
+
+#years = ['2000', '2002', '2004', '2008', '2010', '2012', '2014', '2016']
+years = ['2012']
+
+#datasets = [(v = make_download_table_dicts(years, k, table_key)) for k, v in table_key.items()]
+
+#datasets = [print(v, k) for k, v in table_key.items()]
+
+def download_files(years, table_key):
+	datasets = []
+	for key, value in table_key.items():
+		datasets.append(make_download_table_dicts(years, key, table_key))
+
+	return datasets
+
 
 
 
@@ -97,4 +124,8 @@ def download(data):
 	unzip_rename('ccl', 'zip', ['ccl.txt'])
 	subprocess.call("bash collect_files.sh", shell=True)
 
-#download(datasets)
+
+#Datasets to Download
+datasets = download_files(years, table_key)
+
+download(datasets)
