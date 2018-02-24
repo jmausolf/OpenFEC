5c58d28ae91a9d79c58e3d01e9d7bc5dd4dfde1c
jmausolf <jgm346@gmail.com>
2018-02-23 18:15:06 -0600

    new sql files, adding config

---
+++ fec_download/create_ccl.sql
@@ -0,0 +1,14 @@
+DROP TABLE if exists cand_cmte_link;
+
+CREATE TABLE cand_cmte_link (
+	cand_id TEXT NOT NULL,
+	cand_election_yr NUMERIC NOT NULL,
+	fec_election_yr NUMERIC NOT NULL,
+	cmte_id TEXT,
+	cmte_tp TEXT,
+	cmte_dsgn TEXT,
+	linkage_id NUMERIC NOT NULL
+);
+
+CREATE UNIQUE INDEX idx_cand_cmte_link ON cand_cmte_link (linkage_id);
+
