61a8a0acae1d0e84d42ccf57b7baee707fd525bf
jmausolf <jgm346@gmail.com>
2018-03-15 14:49:21 -0500

    adding rename index to abilities

--- fec_download/create_indiv_cycle.py
+++ fec_download/create_indiv_cycle.py
@@ -1,12 +1,36 @@
-#import pandas as pd
 import sqlite3
 import time
-
+import warnings
 from setlogger import *
-#from build_db import *
 from make_sql import *
 from clean_db import *
 from util import *
+
+
+
+def get_index_rename(db, c, output_table, idx="idx_tmp"):
+
+	idx_new = "idx_{}".format(output_table)
+	get_index_qry = """
+		SELECT sql FROM sqlite_master 
+			WHERE type IS 'index' AND name IS '{}';
+		""".format(idx)
+
+	sqlobj = c.execute(get_index_qry)
+
+	if list(sqlobj) == [] :
+		warnings.warn(
+			"[*] requested idx: {} does not exist..."
+			.format(idx)
+			)
+	else:
+		create_stem = [r[0] for r in c.execute(get_index_qry)][0]
+		create_qry = create_stem.replace(idx, idx_new)+";"
+
+		print("[*] renaming index: {} to {}...".format(idx, idx_new))
+		c.execute("DROP INDEX if exists {};".format(idx))
+		c.execute(create_qry)
+
 
 
 def add_cycle_indiv(db, c, input_table, output_table=None, lim=False):
@@ -47,8 +71,13 @@ def add_cycle_indiv(db, c, input_table, output_table=None, lim=False):
 	else:
 		pass
 
-
+	
+	#Rename Table
 	c.execute(alter_table_rename("tmp", output_table))
+
+	#Rename Index
+	get_index_rename(db, c, "individual_contributions")
+
 	db.commit()
 	print("[*] done")
 
@@ -56,6 +85,7 @@ def add_cycle_indiv(db, c, input_table, output_table=None, lim=False):
 
 if __name__ == "__main__":
 
-	#add_cycle_indiv(db, c, "individual_contributions", "indiv_test", lim=500)
-	#add_cycle_indiv(db, c, "individual_contributions", "indiv_test", lim=500)
-	add_cycle_indiv(db, c, "indiv_test2", "indiv_test2", False)
+	add_cycle_indiv(db, c, "individual_contributions")
+
+
+	#add_cycle_indiv(db, c, "individual_contributions")
