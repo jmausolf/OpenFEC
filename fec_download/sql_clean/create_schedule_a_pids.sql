239cdea043a3723b270548eb75b7208e69fced3e
jmausolf <jgm346@gmail.com>
2018-04-23 20:37:49 -0500

    changing sub id in sa to text, needed for R import

--- fec_download/sql_clean/create_schedule_a_pids.sql
+++ fec_download/sql_clean/create_schedule_a_pids.sql
@@ -51,7 +51,7 @@ CREATE TABLE schedule_a (
 			contributor_file_num NUMERIC,
 			contributor_memo_cd TEXT,
 			contributor_memo_text TEXT,
-			sub_id NUMERIC NOT NULL,
+			sub_id TEXT NOT NULL,
 
 			--requested company
 			--added to tmp after its creation
