f80d3f575d514433703856bf4a473aa3d838e22f
jmausolf <jgm346@gmail.com>
2019-05-03 01:32:56 -0500

    improving pipeline

--- analysis/__RUN__INDIV.R
+++ analysis/__RUN__INDIV.R
@@ -2,6 +2,7 @@
 ## RUN INDIV ANALYSIS
 ####################################
 
+#setwd('~/Box Sync/Dissertation_v2/CH1_OpenFEC/OpenFEC_test_MASTER/analysis/')
 
 ##Load Data Source and Core Functions
 source("indiv_source.R")
@@ -16,11 +17,27 @@ source("indiv_partisan_functions.R")
 source("indiv_desctab.R")
 source("indiv_desctab_appendix.R")
 
+
+#####################################
+## Make Polarization / Sim Measures
+#####################################
+
+source("indiv_make_polarization_similarity_measures.R")
+
+
 #############################
 ## Base Variance Plots
 #############################
 
 source("indiv_vartab_varplot_main.R")
+
+#TODO
+#make plots for skewness, and polarization
+
+#add polarization measures to df_analysis
+#make measures for similarity, Jaquard between firms
+
+
 
 
 #############################
@@ -30,6 +47,14 @@ source("indiv_vartab_varplot_main.R")
 source("hca400_functions.R")
 source("hca400.R")
 
+
+source("indiv_source.R")
+source("indiv_vartab_varplot_functions.R")
+source("indiv_partisan_functions.R")
+source("hca400_loop.R")
+
+source("get_indiv_party_switch.R")
+
 #############################
 ## Post HCA
 #############################
@@ -38,3 +63,6 @@ source("indiv_vartab_varplot_hca.R")
 source("indiv_mean_party_hca.R")
 source("indiv_median_party_hca.R")
 
+
+
+source("indiv_mean_party_hca_loop.R")
--- analysis/indiv_vartab_varplot_main.R
+++ analysis/indiv_vartab_varplot_main.R
@@ -2,33 +2,36 @@
 ## Load SOURCE
 ####################################
 
-source("indiv_source.R")
-source("indiv_vartab_varplot_functions.R")
+# source("indiv_source.R")
+# source("indiv_vartab_varplot_functions.R")
 
 
 #############################
 ## CID MASTER
 #############################
 
-df1_pid <- make_var_df(df_analysis, "cid_master")
-df1_ps <- make_var_df_partisan(df_analysis, "cid_master")
+
+# tab_title1 = "Partisan Polarization - All Firms, Unclassified - Party ID"
+# tab_title2 = "Partisan Polarization - All Firms, Unclassified - Partisan Score"
+
+# vt_all_cidmaster <- var_sum_table(df1_pid, 
+#                                   "output/tables/indiv_var_all_cidmaster_pid_hca_dem.tex",
+#                                   tab_title1)
+# vt_cycle_cidmaster <- var_cycle_table(df1_ps, 
+#                                       "output/tables/indiv_var_cycle_cidmaster_ps_hca_dem.tex", 
+#                                       tab_title2)
+
+
 
 plt_title1 = "Partisan Polarization (PID) - All Firms"
 plt_title2 = "Partisan Polarization (PS) - All Firms"
-tab_title1 = "Partisan Polarization - All Firms, Unclassified - Party ID"
-tab_title2 = "Partisan Polarization - All Firms, Unclassified - Partisan Score"
 
-vt_all_cidmaster <- var_sum_table(df1_pid, 
-                                  "output/tables/indiv_var_all_cidmaster_pid_hca_dem.tex",
-                                  tab_title1)
-vt_cycle_cidmaster <- var_cycle_table(df1_ps, 
-                                      "output/tables/indiv_var_cycle_cidmaster_ps_hca_dem.tex", 
-                                      tab_title2)
+make_polar_graph_base_pid(df_polarization, "polarization_pid", "polarization_pid_base", plt_title1)
+make_polar_graph_base_pid(df_polarization, "polarization_ps", "polarization_ps_base", plt_title2)
+
+make_var_graph_base_pid(df_polarization, "var_pid", "cid_master_pid_base", plt_title1)
+make_var_graph_base_ps(df_polarization, "cid_master_ps_base", plt_title2)
 
 
-
-gr_cid_master_pid <- make_var_graph_base_pid(df1_pid, "cid_master_pid_base", plt_title1)
-gr_cid_master_ps <- make_var_graph_base_ps(df1_ps, "cid_master_ps_base", plt_title2)
-
-
-
+#TODO edit graph code
+make_polar_graph_base_pid(df_polarization, "var_ps", "var_ps_base", plt_title2)
