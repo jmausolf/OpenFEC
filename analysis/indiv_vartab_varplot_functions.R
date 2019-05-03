c2193a6a10aeb39cfa05896cb9c9922996bd6206
jmausolf <jgm346@gmail.com>
2019-05-03 00:46:02 -0500

    adding new polarization graph and graph style

--- analysis/indiv_vartab_varplot_functions.R
+++ analysis/indiv_vartab_varplot_functions.R
@@ -12,6 +12,8 @@ colors_base1 <- pal(2)
 tab_blue = "#1770aa"
 tab_orange = "#fc7d0b"
 
+occ_labels = c("Executives", "Managers", "Others", "All")
+
 
 ####################################
 ## Custom Polarization Function
@@ -76,7 +78,7 @@ polarization <- function(...){
 
 
 ########################################
-#Variance Table + Graph Functions
+#Make PID/PS Var/Sim Measures
 ########################################
 
 ##VARIANCE DF FUNCTION
@@ -173,44 +175,46 @@ make_var_df_partisan <- function(input_df, company_var){
 }
 
 
+########################################
+#Var/Sim Tables Functions
+########################################
 
-
-make_partisan_hist_df <- function(input_df, company_var, party_var){
-  require("dplyr")
-  require("lazyeval")
+# make_partisan_hist_df <- function(input_df, company_var, party_var){
+#   require("dplyr")
+#   require("lazyeval")
   
-  #Get Variance by Organization Levels
+#   #Get Variance by Organization Levels
   
-  selvars = list('cycle', company_var, 'partisan_score', 'occ3')
-  groupvars = list('cycle', company_var, 'occ')
+#   selvars = list('cycle', company_var, 'partisan_score', 'occ3')
+#   groupvars = list('cycle', company_var, 'occ')
   
-  df1a <-  input_df %>%
-    select_(.dots = selvars) %>% 
-    filter(!is.na(partisan_score),
-           !is.na(occ3)) %>%
-    mutate(occ = occ3) %>% 
-    group_by_(.dots = groupvars) 
-
-  
-  #Get Variance All Levels
-  df1b <-  input_df %>%
-    select_(.dots = selvars) %>% 
-    filter(!is.na(partisan_score),
-           !is.na(occ3)) %>%
-    mutate(occ4 = fct_collapse(occ3, ALL = c("CSUITE", "MANAGEMENT", "OTHERS"))) %>%
-    mutate(occ = occ4) %>%
-    group_by_(.dots = groupvars) 
+#   df1a <-  input_df %>%
+#     select_(.dots = selvars) %>% 
+#     filter(!is.na(partisan_score),
+#            !is.na(occ3)) %>%
+#     mutate(occ = occ3) %>% 
+#     group_by_(.dots = groupvars) 
 
   
-  df1 <- rbind(df1a, df1b) %>%
-    #remove "others" from pre 2004, only all exists before levels can be det.
-    mutate(occ = ifelse(occ == "OTHERS" & cycle < 2004, NA, as.character(occ))) %>%
-    mutate(occ = factor(occ,
-                        levels = c("CSUITE", "MANAGEMENT", "OTHERS", "ALL"))) %>%
-    filter(!is.na(occ))
+#   #Get Variance All Levels
+#   df1b <-  input_df %>%
+#     select_(.dots = selvars) %>% 
+#     filter(!is.na(partisan_score),
+#            !is.na(occ3)) %>%
+#     mutate(occ4 = fct_collapse(occ3, ALL = c("CSUITE", "MANAGEMENT", "OTHERS"))) %>%
+#     mutate(occ = occ4) %>%
+#     group_by_(.dots = groupvars) 
+
   
-  return(df1)
-}
+#   df1 <- rbind(df1a, df1b) %>%
+#     #remove "others" from pre 2004, only all exists before levels can be det.
+#     mutate(occ = ifelse(occ == "OTHERS" & cycle < 2004, NA, as.character(occ))) %>%
+#     mutate(occ = factor(occ,
+#                         levels = c("CSUITE", "MANAGEMENT", "OTHERS", "ALL"))) %>%
+#     filter(!is.na(occ))
+  
+#   return(df1)
+# }
 
 
 
@@ -264,19 +268,103 @@ var_cycle_table <- function(df,
 
 
 
+########################################
+# Var/Sim Graph Functions
+########################################
+
 ##VARIANCE GRAPH FUNCTIONS
 
 ##----------------------------  
 ## Base Graph - Pre HCA
+make_polar_graph_base_pid <- function(df_in, key, plt_type="cid_master", plt_title="", plt_caption=""){
 
-make_var_graph_base_pid <- function(df, plt_type="cid_master", plt_title="", plt_caption=""){
+  key = sym(key)
+
+  out_by = paste("by_all_companies", plt_type, sep = "_")
+  outfile <- wout("indiv_plt_partisan_variance_occ", out_by)
+  
+  df <- df_in %>% 
+    group_by(occ, cycle) %>% 
+    summarise(meanvar = mean(!!key, na.rm = T)) %>%
+    filter(!is.nan(meanvar)) %>% 
+    mutate(polarization = meanvar)
+  
+  g <- ggplot(df, aes(make_datetime(cycle), polarization)) +
+  geom_smooth(color="#3A084A", alpha=0.15, size=0.5) +
+  geom_line(aes(color=occ), alpha=0.9) +
+  
+  #Add Point to Add the Shape by Occ
+  geom_point(aes(shape=occ), alpha=1, size=3) +
+
+  #Fill Each Occ Shape / Get Outline Independently
+  geom_point(data = df %>% filter(occ == "CSUITE"), shape=21, alpha=1,
+             pch=21, size=3, fill=colors_base[1]) +
+  geom_point(data = df %>% filter(occ == "MANAGEMENT"), shape=22, alpha=1,
+             pch=21, size=3, fill=colors_base[2]) +
+  geom_point(data = df %>% filter(occ == "OTHERS"), shape=23, alpha=1,
+             pch=21, size=3, fill=colors_base[3]) +
+  geom_point(data = df %>% filter(occ == "ALL"), shape=24, alpha=1,
+             pch=21, size=3, fill=colors_base[4]) +
+  
+    #Add bbcstyle
+  bbc_style() +
+  
+  #Manual Scales
+  scale_color_manual("", values=colors_base, labels=occ_labels) +
+  scale_shape_manual("", values=c(21, 22, 23, 24), labels=occ_labels) +
+  scale_x_datetime(date_labels = "%Y", date_breaks = "2 year") +
+  
+  #Xaxis Line
+  geom_hline(yintercept = 0.0, size = 1, colour="#333333") +
+  
+  #Add axis titles
+  theme(axis.title = element_text(size = 18)) +
+  xlab("Contribution Cycle") +
+  ylab("Partisan Polarization [0, 1]") +
+  labs(title = plt_title,
+       caption = plt_caption) +
+  theme(plot.title = element_text(hjust = 0.5)) +
+  
+  #Adjust Legend Position
+  theme(
+    legend.justification='left',
+    legend.direction='vertical',
+    legend.position=c(0.0,.9)
+  ) +
+  
+  #Add x axis ticks
+  theme(
+    axis.ticks.x = element_line(colour = "#333333"), 
+    axis.ticks.length =  unit(0.26, "cm"),
+    axis.text = element_text(size=10, color="#222222")) +
+
+  #Override the Legend Fill
+  guides(shape = guide_legend(override.aes = list(fill = colors_base)))
+  
+  finalise_plot(g, plt_caption, outfile, footer=FALSE)
+  return(g)
+  
+}
+
+
+
+make_var_graph_base_pid <- function(df, key, plt_type="cid_master", plt_title="", plt_caption=""){
+
+  key = sym(key)
+  #require("dplyr")
+  #require("lazyeval")
+  
+  #Get Variance by Organization Levels
+  
+  #keyvar = list(key)
 
   out_by = paste("by_all_companies", plt_type, sep = "_")
   outfile <- wout("indiv_plt_partisan_variance_occ", out_by)
   
   df_var_cycle_graph <- df %>% 
     group_by(occ, cycle) %>% 
-    summarise(meanvar = mean(var_pid, na.rm = T)) %>%
+    summarise(meanvar = mean(!!key, na.rm = T)) %>%
+    #summarise(meanvar = mean(key, na.rm = T)) %>%
     filter(!is.nan(meanvar)) %>% 
     mutate(polarization = 1-meanvar)
   
